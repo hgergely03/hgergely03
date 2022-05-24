8d03413fe3364eac5bdeacf2d5c2f7d714f6453a
Horváth Gergely <gergely0703@gmail.com>
2022-05-24 14:30:02 +0200

    Add SvelteKit skeleton

---
+++ portfolio-site/src/lib/Navbar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	
+</script>
+
+<!-- 
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
+			<li class:active={$page.url.pathname === '/about'}>
+				<a sveltekit:prefetch href="/about">About</a>
+			</li>
+			<li class:active={$page.url.pathname === '/todos'}>
+				<a sveltekit:prefetch href="/todos">Todos</a>
+			</li>
+ -->
+<header>
+	
+</header>
+
+<style lang="scss">
+	
+</style>
