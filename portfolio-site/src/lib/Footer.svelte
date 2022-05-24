61394d2f9f3f45b6865acff4ffaa01e7712759e2
Horváth Gergely <gergely0703@gmail.com>
2022-05-24 20:53:55 +0200

    Create footer & begin working on hero

--- portfolio-site/src/lib/Footer.svelte
+++ portfolio-site/src/lib/Footer.svelte
@@ -1,122 +1,46 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 </script>
 
-<header>
-	<div class="corner">
-		<a href="https://kit.svelte.dev">
-		</a>
+<footer>
+	<div class="container">
+		<div class="row justify-content-md-center">
+			<div class="col-md-auto"><a href="https://github.com/hgergely03">GitHub</a></div>
+			<!-- <div class="col-md-auto"><a href="https://github.com/hgergely03">GitHub</a></div>
+			<div class="col-md-auto"><a href="https://github.com/hgergely03">GitHub</a></div> -->
+		</div>
+		<div class="row mt-4">
+			<p>&copy; Horváth Gergely 2022</p>
+		</div>
 	</div>
+</footer>
 
-	<nav>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-		</svg>
-		<ul>
-			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.url.pathname === '/about'}>
-				<a sveltekit:prefetch href="/about">About</a>
-			</li>
-			<li class:active={$page.url.pathname === '/todos'}>
-				<a sveltekit:prefetch href="/todos">Todos</a>
-			</li>
-		</ul>
-		<svg viewBox="0 0 2 3" aria-hidden="true">
-			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-		</svg>
-	</nav>
-
-	<div class="corner">
-		<!-- TODO put something else here? github link? -->
-	</div>
-</header>
-
-<style>
-	header {
-		display: flex;
-		justify-content: space-between;
+<style lang="scss">
+	footer {
+		background-color: $hero-background;
+		background-color: var(--hero-background);
+		color: $light-text-color;
+		color: var(--light-text-color);
+		text-align: center;
+		margin-top: auto;
 	}
 
-	.corner {
-		width: 3em;
-		height: 3em;
-	}
-
-	.corner a {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		width: 100%;
-		height: 100%;
-	}
-
-	.corner img {
-		width: 2em;
-		height: 2em;
-		object-fit: contain;
-	}
-
-	nav {
-		display: flex;
-		justify-content: center;
-		--background: rgba(255, 255, 255, 0.7);
-	}
-
-	svg {
-		width: 2em;
-		height: 3em;
-		display: block;
-	}
-
-	path {
-		fill: var(--background);
-	}
-
-	ul {
-		position: relative;
-		padding: 0;
-		margin: 0;
-		height: 3em;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		list-style: none;
-		background: var(--background);
-		background-size: contain;
-	}
-
-	li {
-		position: relative;
-		height: 100%;
-	}
-
-	li.active::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--accent-color);
-	}
-
-	nav a {
-		display: flex;
-		height: 100%;
-		align-items: center;
-		padding: 0 1em;
-		color: var(--heading-color);
-		font-weight: 700;
-		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
+	a {
+		padding-left: 24px;
+		padding-right: 24px;
+		font-size: 24px;
+		color: $light-text-color;
+		color: var(--light-text-color);
 		text-decoration: none;
-		transition: color 0.2s linear;
 	}
 
-	a:hover {
-		color: var(--accent-color);
+	p {	
+		font-size: 20px;
+		font-weight: 200;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 20px 0;
+		}
 	}
 </style>
--- portfolio-site/src/routes/__layout.svelte
+++ portfolio-site/src/routes/__layout.svelte
@@ -1,45 +1,18 @@
 <script lang="ts">
 	import Navbar from '$lib/Navbar.svelte';
+	import Footer from '$lib/Footer.svelte';
+	import 'bootstrap/dist/css/bootstrap.min.css';
 	import '../app.scss';
 </script>
 
-<Navbar />
+<Navbar/>
 
 <main>
 	<slot />
 </main>
 
-<footer>
-	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-</footer>
+<Footer/>
 
 <style>
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 1024px;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 40px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 40px 0;
-		}
-	}
+	
 </style>
