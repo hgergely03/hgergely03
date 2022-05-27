685aac8bf0c6387a3a8d5f4ab9277d7db7b68527
Horváth Gergely <gergely0703@gmail.com>
2022-05-27 22:36:49 +0200

    Fix .nojekyll

--- portfolio-site/svelte.config.js
+++ portfolio-site/svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = "production" === "development";
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -18,10 +16,6 @@ const config = {
 			pages: 'docs',
 			assets: 'docs'
 		}),
-		paths: {
-            // change below to your repo name
-            base: dev ? "/" : "/hgergely03",
-        },
 		vite: {
 			css: {
 				preprocessorOptions: {
