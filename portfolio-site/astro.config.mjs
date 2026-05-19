e7ce05d24244e0e7daf3c4931958d8c3c962404b
Horváth Gergely <gergely0703@gmail.com>
2026-05-19 13:39:14 +0200

    Add sitewide font

--- portfolio-site/astro.config.mjs
+++ portfolio-site/astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +8,18 @@ export default defineConfig({
   image: {
       responsiveStyles: true
   },
+
+  fonts: [{
+    provider: fontProviders.local(),
+    name: "Inter",
+    cssVariable: "--font-inter",
+    options: {
+      variants: [{
+        src: ['./src/assets/fonts/Inter-Variable.ttf'],
+        weight: "100 900",
+        style: 'normal'
+      }]
+    },
+    fallbacks: ["sans-serif"]
+  }]
 });
